cc0d83badf2bbdf276af47caef13aba4dd47df7a
Satish <satishchoudhary642@gmail.com>
2026-02-10 19:44:18 +0530

    Initial commit: Set up Spring Boot Manager structure

---
+++ pixel-pull manager/src/test/java/com/pixelpull/pixel_pull/PixelPullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelpull.pixel_pull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelPullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
