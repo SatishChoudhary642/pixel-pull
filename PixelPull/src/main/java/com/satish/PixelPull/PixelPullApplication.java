b6fed38ae31f22dedf062a9f00880b00fdfd6756
Satish <satishchoudhary642@gmail.com>
2026-07-06 18:33:03 +0530

    fix: recreate main class, remove spring-ai, disable docker-compose auto-start

---
+++ PixelPull/src/main/java/com/satish/PixelPull/PixelPullApplication.java
@@ -0,0 +1,13 @@
+package com.satish.pixelpull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelPullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelPullApplication.class, args);
+	}
+
+}
